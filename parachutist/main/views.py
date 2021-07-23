e84e6d67c4f73bc1112f19874603acaf9cb48209
Владислав Ан <anvladic13@gmail.com>
2021-07-23 16:16:20 +0300

    add services, booking page

--- parachutist/main/views.py
+++ parachutist/main/views.py
@@ -7,3 +7,11 @@ def index(request):
 
 def gallery(request):
     return render(request, 'gallery.html')
+
+
+def services(request):
+    return render(request, 'services.html')
+
+
+def booking(request):
+    return render(request, 'booking.html')
