02fa944bfb454b55f2ae385795ae0a641c7891a9
Владислав Ан <anvladic13@gmail.com>
2021-07-24 14:02:14 +0300

    fix room model

--- parachutist/parachutist/urls.py
+++ parachutist/parachutist/urls.py
@@ -17,10 +17,16 @@ from django.contrib import admin
 from django.urls import path
 import main.views as v
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', v.index),
     path('gallery', v.gallery),
     path('services', v.services),
     path('booking', v.booking),
+    path('info/debtors-notebook', v.debtors),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
